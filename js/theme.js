fed0bb2a250309b6e2f758662a457631e1fbd093
Matheus Camargo <theus.xalmeida@gmail.com>
2026-09-04 01:06:28 -0300

    estilizando/button/toogle/tema

--- js/theme.js
+++ js/theme.js
@@ -1,8 +1,13 @@
 const botaoTema = document.querySelector('#botao-tema')
 
 export function iniciarTema() {
-    botaoTema.addEventListener('click', () => {
-        document.body.classList.toggle('tema-claro')
-        console.log('Tema alterado')
+
+    botaoTema.addEventListener('change', () => {
+
+        document.body.classList.toggle(
+            'tema-claro',
+            botaoTema.checked
+        )
+
     })
 }
